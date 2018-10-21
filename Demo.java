289feb422a30652cda9eaf9efd0bb45e73739f0e
ShettyKanika0506 <shettykanika@ymail.com>
2018-10-21 21:35:47 +0530

    commit the file creation program

---
+++ Demo.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Demo extends M {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+
+		Demo d = new Demo();
+		try{
+		d.method();
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+}
+}
---
+++ filecreation.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+public class filecreation {
+
+	public void file() throws IOException
+	{
+		File f=new File("C:\\Users\\kanika\\Documents\\sample.txt");
+		boolean b=f.createNewFile();
+		
+System.out.println(b);
+		
+	}
+	
+	public static void main(String[]args) throws IOException
+	{
+		filecreation ff=new filecreation();
+		ff.file();
+	}
+	
+}
