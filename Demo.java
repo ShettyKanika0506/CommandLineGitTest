4c7b1ff3936aecf173ee43e6459b4e0586404086
ShettyKanika0506 <43107029+ShettyKanika0506@users.noreply.github.com>
2018-10-27 20:45:50 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -4,7 +4,7 @@ public class Demo extends M {
 
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-
+System.out.println("My demo program");
 		Demo d = new Demo();
 		try{
 		d.method();
